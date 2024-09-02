aa629b87e6a6a60f953ce72a25ae5c3425c19025
chavesef <efchaves@uolinc.com>
2024-09-02 15:43:11 -0300

    feat: criando classe para receber o corpo da requisição

---
+++ src/main/java/com/dape/api/adapter/dto/BetPostRequest.java
@@ -0,0 +1,30 @@
+package com.dape.api.adapter.dto;
+
+import java.math.BigDecimal;
+
+public class BetPostRequest {
+
+    private BigDecimal numOdd;
+    private String desBet;
+
+    public BetPostRequest(BigDecimal numOdd, String desBet) {
+        this.numOdd = numOdd;
+        this.desBet = desBet;
+    }
+
+    public BigDecimal getNumOdd() {
+        return numOdd;
+    }
+
+    public void setNumOdd(BigDecimal numOdd) {
+        this.numOdd = numOdd;
+    }
+
+    public String getDesBet() {
+        return desBet;
+    }
+
+    public void setDesBet(String desBet) {
+        this.desBet = desBet;
+    }
+}
