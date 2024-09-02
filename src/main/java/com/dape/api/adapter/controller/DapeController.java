3e3b43c2ad45b1188e4eb81aa4cfd26b1196d016
chavesef <efchaves@uolinc.com>
2024-09-02 15:43:50 -0300

    feat: adicionando mapping para método POST para criar uma aposta

--- src/main/java/com/dape/api/adapter/controller/DapeController.java
+++ src/main/java/com/dape/api/adapter/controller/DapeController.java
@@ -1,14 +1,35 @@
 package com.dape.api.adapter.controller;
 
+import com.dape.api.adapter.dto.BetPostRequest;
+import com.dape.api.domain.entity.Bet;
+import com.dape.api.usecase.service.BetService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/dape")
 public class DapeController {
+
+    private final BetService betService;
+
+    public DapeController(BetService betService) {
+        this.betService = betService;
+    }
+
     @GetMapping("/helloworld")
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/bet")
+    public Bet cadastrarAposta(@RequestBody BetPostRequest betPostRequest){
+        return betService.cadastrarAposta(betPostRequest);
     }
 }
