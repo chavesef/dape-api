294b31b3357067c4d5d2bc53f617835fe7b24f13
chavesef <efchaves@uolinc.com>
2024-09-04 10:37:19 -0300

    feat: criando classe dto para resposta à requisição POST no endpoint /bet

---
+++ src/main/java/com/dape/api/adapter/dto/BetPostResponse.java
@@ -0,0 +1,63 @@
+package com.dape.api.adapter.dto;
+
+import com.dape.api.domain.enums.BetStatusEnum;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class BetPostResponse {
+    private Long idtBet;
+    private String desBet;
+    private BigDecimal numOdd;
+    private LocalDate datCreated;
+    private BetStatusEnum betStatus;
+
+    public BetPostResponse(Long idtBet, String desBet, BigDecimal numOdd,
+                           LocalDate datCreated, BetStatusEnum betStatus) {
+        this.idtBet = idtBet;
+        this.desBet = desBet;
+        this.numOdd = numOdd;
+        this.datCreated = datCreated;
+        this.betStatus = betStatus;
+    }
+
+    public Long getIdtBet() {
+        return idtBet;
+    }
+
+    public void setIdtBet(Long idtBet) {
+        this.idtBet = idtBet;
+    }
+
+    public String getDesBet() {
+        return desBet;
+    }
+
+    public void setDesBet(String desBet) {
+        this.desBet = desBet;
+    }
+
+    public BigDecimal getNumOdd() {
+        return numOdd;
+    }
+
+    public void setNumOdd(BigDecimal numOdd) {
+        this.numOdd = numOdd;
+    }
+
+    public LocalDate getDatCreated() {
+        return datCreated;
+    }
+
+    public void setDatCreated(LocalDate datCreated) {
+        this.datCreated = datCreated;
+    }
+
+    public BetStatusEnum getBetStatus() {
+        return betStatus;
+    }
+
+    public void setBetStatus(BetStatusEnum betStatus) {
+        this.betStatus = betStatus;
+    }
+}
