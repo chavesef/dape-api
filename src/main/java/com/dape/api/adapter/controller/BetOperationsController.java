60122f2699b74069ea3d821e0a64d50d9a87e4d2
chavesef <efchaves@uolinc.com>
2024-09-05 15:29:40 -0300

    feat: removendo HelloWorld e alterando nomes da classe e do método

--- src/main/java/com/dape/api/adapter/controller/BetOperationsController.java
+++ src/main/java/com/dape/api/adapter/controller/BetOperationsController.java
@@ -4,7 +4,6 @@ import com.dape.api.adapter.dto.BetPostRequest;
 import com.dape.api.adapter.dto.BetPostResponse;
 import com.dape.api.usecase.service.BetService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dape")
-public class DapeController {
+public class BetOperationsController {
 
     private final BetService betService;
 
-    public DapeController(BetService betService) {
+    public BetOperationsController(BetService betService) {
         this.betService = betService;
-    }
-
-    @GetMapping("/helloworld")
-    public String helloWorld(){
-        return "Hello World!";
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/bet")
-    public BetPostResponse cadastrarAposta(@RequestBody BetPostRequest betPostRequest){
+    public BetPostResponse registerBet(@RequestBody BetPostRequest betPostRequest){
         return betService.cadastrarAposta(betPostRequest);
     }
 }
