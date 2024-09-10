e0d6363e05dafbc188ac28ebb6c67e2b3ea963c2
chavesef <efchaves@uolinc.com>
2024-09-10 17:13:05 -0300

    refactor: alterando nome da classe converter para nome genérico

--- src/test/java/com/dape/api/cucumber/DataTableConverter.java
+++ src/test/java/com/dape/api/cucumber/DataTableConverter.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
 
-public class BetConverter {
+public class DataTableConverter {
 
     @DataTableType
     public Bet betDataTableType(Map<String, String> map){
