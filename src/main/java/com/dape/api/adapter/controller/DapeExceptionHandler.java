11d14b9022314ef3c37e28afdd2f077b7046fb17
chavesef <efchaves@uolinc.com>
2024-09-20 14:39:30 -0300

    feat: adicionando as novas exceções criadas para serem tratadas pelo ExceptionHandler

--- src/main/java/com/dape/api/adapter/controller/DapeExceptionHandler.java
+++ src/main/java/com/dape/api/adapter/controller/DapeExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.dape.api.adapter.controller;
 
 import com.dape.api.adapter.dto.response.ErrorResponse;
+import com.dape.api.domain.exception.BetNotExistent;
+import com.dape.api.domain.exception.BetSelectedOrResolvedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -27,6 +29,16 @@ public class DapeExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        return ResponseEntity.status(400).body(new ErrorResponse(400, e.getMessage()));
+    }
+
+    @ExceptionHandler(BetNotExistent.class)
+    public ResponseEntity<ErrorResponse> handleBetNotExistException(BetNotExistent e){
+        return ResponseEntity.status(404).body(new ErrorResponse(404, e.getMessage()));
+    }
+
+    @ExceptionHandler(BetSelectedOrResolvedException.class)
+    public ResponseEntity<ErrorResponse> handleBetSelectedException(BetSelectedOrResolvedException e){
         return ResponseEntity.status(400).body(new ErrorResponse(400, e.getMessage()));
     }
 }
