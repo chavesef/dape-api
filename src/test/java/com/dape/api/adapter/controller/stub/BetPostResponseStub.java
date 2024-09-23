e0cbb68ce725094143416b8b348ea05002ac2bef
chavesef <efchaves@uolinc.com>
2024-09-23 10:30:17 -0300

    refactor: alterando nome da classe stub de response para nome genérico

--- src/test/java/com/dape/api/adapter/controller/stub/BetPostResponseStub.java
+++ src/test/java/com/dape/api/adapter/controller/stub/BetPostResponseStub.java
@@ -4,11 +4,26 @@ import com.dape.api.adapter.dto.response.BetResponse;
 import com.dape.api.domain.entity.Bet;
 
 import static com.dape.api.adapter.controller.stub.BetStub.createBet;
+import static com.dape.api.adapter.controller.stub.BetStub.createUpdatedBet;
 
 public class BetPostResponseStub {
 
     public static BetResponse createBetPostResponse() {
         Bet bet = createBet();
+
+        return new BetResponse(
+                bet.getIdtBet(),
+                bet.getDesBet(),
+                bet.getNumOdd(),
+                bet.getDatCreated(),
+                bet.getDatUpdated(),
+                bet.getBetStatusEnum(),
+                bet.getFlgSelected()
+        );
+    }
+
+    public static BetResponse createBetPatchResponse() {
+        Bet bet = createUpdatedBet();
 
         return new BetResponse(
                 bet.getIdtBet(),
