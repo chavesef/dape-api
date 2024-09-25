48cca8722e8d42322d20470badba4234bd1549c1
chavesef <efchaves@uolinc.com>
2024-09-25 16:55:54 -0300

    feat: adicionando Logger à classe controller

--- src/main/java/com/dape/api/adapter/controller/BetOperationsController.java
+++ src/main/java/com/dape/api/adapter/controller/BetOperationsController.java
@@ -5,6 +5,8 @@ import com.dape.api.adapter.dto.response.BetResponse;
 import com.dape.api.usecase.factory.BetResponseFactory;
 import com.dape.api.usecase.service.BetService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,12 +16,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/dape")
 public class BetOperationsController {
 
     private final BetService betService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(BetOperationsController.class);
 
     public BetOperationsController(BetService betService) {
         this.betService = betService;
@@ -27,13 +29,15 @@ public class BetOperationsController {
 
     @PostMapping("/bet")
     public ResponseEntity<BetResponse> registerBet(@Valid @RequestBody BetRequest betRequest){
+        LOGGER.info("Método POST chamado para cadastrar uma nova aposta com descrição={} e odd={}", betRequest.getDesBet(), betRequest.getNumOdd());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(BetResponseFactory.createBetResponse(betService.registerBet(betRequest)));
     }
 
     @PatchMapping("/bet/{idtBet}")
     public ResponseEntity<BetResponse> updateBet(@PathVariable Long idtBet, @Valid @RequestBody BetRequest betRequest){
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(BetResponseFactory.createBetResponse(betService.updateBet(idtBet, betRequest)));
+        LOGGER.info("Método PATCH chamado para atualizar uma aposta com id={} e descrição={} e odd={}", idtBet, betRequest.getDesBet(), betRequest.getNumOdd());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(BetResponseFactory.createBetResponse(betService.updateBet(idtBet, betRequest)));
     }
 }
