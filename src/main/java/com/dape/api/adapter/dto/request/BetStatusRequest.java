3323fe951245af52c71d1619da51a38bdec4b416
chavesef <efchaves@uolinc.com>
2024-10-07 16:21:43 -0300

    feat: criando classe dto para receber a solicitação de atualização de status

---
+++ src/main/java/com/dape/api/adapter/dto/request/BetStatusRequest.java
@@ -0,0 +1,20 @@
+package com.dape.api.adapter.dto.request;
+
+import com.dape.api.domain.enums.BetStatusEnum;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+
+public class BetStatusRequest {
+
+    private BetStatusEnum betStatus;
+
+    public BetStatusEnum getBetStatus() {
+        return betStatus;
+    }
+
+    public void setBetStatus(BetStatusEnum betStatus) {
+        this.betStatus = betStatus;
+    }
+}
