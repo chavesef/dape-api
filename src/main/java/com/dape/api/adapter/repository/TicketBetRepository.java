e0beca7638bcd6255c617db45f42c4c218bfa16f
chavesef <echaves1@uolinc.com>
2025-04-03 17:00:00 -0300

    feat: adicionando querys aos repositórios de bilhetes

--- src/main/java/com/dape/api/adapter/repository/TicketBetRepository.java
+++ src/main/java/com/dape/api/adapter/repository/TicketBetRepository.java
@@ -1,7 +1,11 @@
 package com.dape.api.adapter.repository;
 
 import com.dape.api.domain.entity.TicketBet;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TicketBetRepository extends JpaRepository<TicketBet, Long> {
+    @Query(value = "select idt_bet from ticket_bet where idt_ticket = :ticketId", nativeQuery = true)
+    List<Long> findBetsByTicketId(Long ticketId);
 }
